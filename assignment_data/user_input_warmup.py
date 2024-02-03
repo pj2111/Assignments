465213a0fc3adce218074c918c81baacb69628d6
kamalabot <kamaljp@gmail.com>
2024-02-03 08:44:31 +0530

    starting math module

---
+++ assignment_data/user_input_warmup.py
@@ -0,0 +1,29 @@
+# warmup that can be executed only as script, so read all the instruction first
+# provide appropriate instruction to the user on the format of the input
+# ensure the entire script runs, including all logic
+
+# 1 ask user to provide 5 words that can be used as variable name,
+
+# 2 print the vaiable names provided by the user in earlier step
+
+# 3 Check if all the provided variable names are valid python variable names
+# Extract those names, that are invalid into invalid_names list, print it
+
+# 4 Extract the variable names that are more than 4 chars into sep1, print it
+
+# 5 ask for the user one variable name, 
+
+# 6 Make the script to continue asking for one more variable name, endlessly
+
+# 7 After breaking from endless loop, now re-write the endless var name request
+# with a break condition
+
+# Inform user that, this script can accept command line arguments
+
+# Implement simple logic to check if there is a command line argument
+
+# parse the command line arguments provided by the user in to cli variable
+
+# Implement Logic to check how many c-line args are provided, and ask user to provide minimum 5
+
+# Do the 2 / 3 and 4 instruction above
